0a9c489541e6b540a0c6b9ae8ae21ec06eeffb09
Ujjwal Kumar <96404713+UjjwalKumar7209@users.noreply.github.com>
2025-06-12 14:47:39 +0530

    Create Sliding_Window_Maximum.cpp

---
+++ Stack_Queue/Sliding_Window_Maximum.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Sliding Window Maximum (LeetCode #239)
+Link: https://leetcode.com/problems/sliding-window-maximum/
+Approach: Use deque to store indices of useful elements for the current window in decreasing order.
+Time: O(n), Space: O(k)
+Date: June 12, 2025
+*/
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+      vector<int> list;
+      deque<pair<int, int>> dq;
+      for (int i = 0; i < nums.size(); i++) {
+          if (!dq.empty() && dq.front().first <= i-k) {
+              dq.pop_front();
+          }
+          while (!dq.empty() && dq.back().second <= nums[i]) {
+              dq.pop_back();
+          }
+          dq.push_back({i, nums[i]});
+          if (i >= k-1) list.push_back(dq.front().second);
+      }
+      return list;
+  }
