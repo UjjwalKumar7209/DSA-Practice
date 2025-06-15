d2c5cc33d59b1bd078259447548c437f374ec889
Ujjwal Kumar <96404713+UjjwalKumar7209@users.noreply.github.com>
2025-06-15 13:12:45 +0530

    Create Preorder_Traversal.cpp

---
+++ Binary_Tree/Preorder_Traversal.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Binary Tree Preorder Traversal (LeetCode #144)
+Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+Approach: Root → Left → Right; implemented using recursion or iterative stack.
+Time: O(n), Space: O(n)
+Date: June 11, 2025
+*/
+
+
+class PreOrder {
+private:
+    void preorderTraversalCode(vector<int> &ans, TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        ans.push_back(root->val);
+        preorderTraversalCode(ans, root->left);
+        preorderTraversalCode(ans, root->right);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preorderTraversalCode(ans, root);
+        return ans;
+    }
+};
