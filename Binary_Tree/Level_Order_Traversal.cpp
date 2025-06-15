4c8c5519744ab1e6a1a115e376a00964d7800a54
Ujjwal Kumar <96404713+UjjwalKumar7209@users.noreply.github.com>
2025-06-15 13:15:13 +0530

    Create Level_Order_Traversal.cpp

---
+++ Binary_Tree/Level_Order_Traversal.cpp
@@ -0,0 +1,8 @@
+/*
+Problem: Binary Tree Level Order Traversal (LeetCode #102)
+Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+Approach: Use a queue to traverse each level of the tree iteratively.
+Time: O(n), Space: O(n)
+Date: June 15, 2025
+*/
+
