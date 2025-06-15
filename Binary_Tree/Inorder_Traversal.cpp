800fe8f73e42dbac070604fd05d6944593581645
Ujjwal Kumar <96404713+UjjwalKumar7209@users.noreply.github.com>
2025-06-15 13:13:26 +0530

    Create Inorder_Traversal.cpp

---
+++ Binary_Tree/Inorder_Traversal.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Binary Tree Inorder Traversal (LeetCode #94)
+Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+Approach: Left → Root → Right; implemented using recursion or iterative stack.
+Time: O(n), Space: O(n)
+Date: June 11, 2025
+*/
+
+
+class Solution {
+private:
+    void inorderTraversalCode(vector<int> &ans, TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        inorderTraversalCode(ans, root->left);
+        ans.push_back(root->val);
+        inorderTraversalCode(ans, root->right);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorderTraversalCode(ans, root);
+        return ans;
+    }
+};
